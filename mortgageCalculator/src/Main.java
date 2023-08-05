93d1e32207fb320e9f9339c36cd99224affa78f3
alhassanmoses <alhassanmoses.amw@gmail.com>
2023-08-05 13:36:54 +0000

    Fixed some logical errors with the mortgage calculation logic

--- mortgageCalculator/src/Main.java
+++ mortgageCalculator/src/Main.java
@@ -6,22 +6,20 @@ public class Main {
        Scanner scanner = new Scanner(System.in);
         System.out.print("Please input the following in the order of the names presented. \nPrincipal: ");
         long principal = scanner.nextLong();
-        System.out.print("\nAnnual Interest Rate: ");
+        System.out.print("Annual Interest Rate: ");
         float annualInterestRate = (scanner.nextFloat() / 100) / 12;
-        System.out.print("\nPeriod (Years): ");
-        int period = scanner.nextInt();
+        System.out.print("Period (Years): ");
+        int period = scanner.nextInt() * 12;
 
         double mortgage;
         double numerator;
         double denominator;
+
         numerator = annualInterestRate * Math.pow((1 + annualInterestRate), period);
         denominator = Math.pow ((1 + annualInterestRate), period) - 1;
 
-        mortgage = numerator/denominator;
-
-
-
-        System.out.print("\nMortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+        mortgage = principal * ( numerator/denominator );
+        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
 
     }
 }
