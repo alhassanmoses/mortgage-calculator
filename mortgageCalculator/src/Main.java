986a564adc415e97bf14d4ffa3b73b489ff7888e
alhassanmoses <alhassanmoses.amw@gmail.com>
2023-08-05 15:30:42 +0000

    Added some conditional logic to enforce a few restriction.

--- mortgageCalculator/src/Main.java
+++ mortgageCalculator/src/Main.java
@@ -5,14 +5,37 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_IN_YEAR = 12;
         final byte TOTAL_PERCENT = 100;
+        long principal;
+        float annualInterestRate;
+        int paymentPeriod;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Please input the following in the order of the names presented. \nPrincipal: ");
-        long principal = scanner.nextLong();
+
+        System.out.print("Please input the following in the order of the names presented. \nPrincipal ($1K - $10M): ");
+        while (true){
+            principal = scanner.nextLong();
+            if (principal >= 1000 && principal <= 10_000_000)
+                break;
+
+            System.out.print("Please enter a number between the ranges 1,000 - 10,000,000.\nPrincipal ($1K - $10M): ");
+        }
+
         System.out.print("Annual Interest Rate: ");
-        float annualInterestRate = (scanner.nextFloat() / TOTAL_PERCENT);
-        float monthlyInterestRate = annualInterestRate / MONTHS_IN_YEAR;
+        while (true){
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate >= 1 && annualInterestRate <= 30)
+                break;
+            System.out.print("Please enter a value greater than 0 and less than or equal to 30. \nAnnual Interest Rate: ");
+        }
+        float monthlyInterestRate = ( annualInterestRate / TOTAL_PERCENT) / MONTHS_IN_YEAR;
+
         System.out.print("Period (Years): ");
-        int paymentPeriod = scanner.nextInt() * MONTHS_IN_YEAR;
+        while (true){
+            paymentPeriod = scanner.nextInt();
+            if (paymentPeriod >=1 && paymentPeriod <=30)
+                break;
+            System.out.print("Please input a value between 1 - 30.\n Period (Years): ");
+        }
+        paymentPeriod *= MONTHS_IN_YEAR;
 
         double mortgage;
         double numerator;
