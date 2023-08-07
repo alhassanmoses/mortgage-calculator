11e0688d352ab478ff9c49b918fbb23aaf79a296
alhassanmoses <alhassanmoses.amw@gmail.com>
2023-08-07 01:07:39 +0000

    Created a method to hold the core logic, will update the control flows later

--- mortgageCalculator/src/Main.java
+++ mortgageCalculator/src/Main.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte TOTAL_PERCENT = 100;
+
         long principal;
         float annualInterestRate;
-        int paymentPeriod;
+        byte paymentPeriod;
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Please input the following in the order of the names presented. \nPrincipal ($1K - $10M): ");
@@ -15,7 +14,6 @@ public class Main {
             principal = scanner.nextLong();
             if (principal >= 1000 && principal <= 10_000_000)
                 break;
-
             System.out.print("Please enter a number between the ranges 1,000 - 10,000,000.\nPrincipal ($1K - $10M): ");
         }
 
@@ -26,26 +24,40 @@ public class Main {
                 break;
             System.out.print("Please enter a value greater than 0 and less than or equal to 30. \nAnnual Interest Rate: ");
         }
-        float monthlyInterestRate = ( annualInterestRate / TOTAL_PERCENT) / MONTHS_IN_YEAR;
 
         System.out.print("Period (Years): ");
         while (true){
-            paymentPeriod = scanner.nextInt();
+            paymentPeriod = scanner.nextByte();
             if (paymentPeriod >=1 && paymentPeriod <=30)
                 break;
-            System.out.print("Please input a value between 1 - 30.\n Period (Years): ");
+            System.out.print("Please input a value between 1 - 30.\nPeriod (Years): ");
         }
-        paymentPeriod *= MONTHS_IN_YEAR;
+
+        double  mortgage = calculateMortgage(principal, annualInterestRate, paymentPeriod);
+
+        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+
+    }
+
+    public static double calculateMortgage(long principal, float annualInterestRate, byte paymentPeriod ){
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte TOTAL_PERCENT = 100;
 
         double mortgage;
         double numerator;
         double denominator;
 
-        numerator = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), paymentPeriod);
-        denominator = Math.pow ((1 + monthlyInterestRate), paymentPeriod) - 1;
+        double monthlyInterestRate = ( annualInterestRate / TOTAL_PERCENT) / MONTHS_IN_YEAR;
+        short numberOfPayments = (short)(paymentPeriod * MONTHS_IN_YEAR);
+
+
+        numerator = monthlyInterestRate * Math.pow((1 + monthlyInterestRate), numberOfPayments);
+        denominator = Math.pow ((1 + monthlyInterestRate), numberOfPayments) - 1;
 
         mortgage = principal * ( numerator/denominator );
-        System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
+
+        return mortgage;
 
     }
 }
